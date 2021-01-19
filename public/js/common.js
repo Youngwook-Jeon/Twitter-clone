d73bf15aaf2fa4ce5c204b39e8f0d0f903af39d6
Young wook Jeon <ela87@naver.com>
2021-01-19 22:16:31 +0900

    Upload Profile Pictures

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,6 @@
+// Globals
+var cropper;
+
 $("#postTextarea, #replyTextarea").keyup((event) => {
   var textbox = $(event.target);
   var value = textbox.val().trim();
@@ -79,6 +82,51 @@ $("#deletePostButton").click((event) => {
     }
   });
 });
+
+$("#filePhoto").change(function() {
+  if (this.files && this.files[0]) {
+    var reader = new FileReader();
+    reader.onload = (e) => {
+      var image = document.getElementById("imagePreview");
+      image.src = e.target.result;
+
+      if (cropper !== undefined) {
+        cropper.destroy();
+      }
+
+      cropper = new Cropper(image, {
+        aspectRatio: 1 / 1,
+        background: false
+      });
+    }
+    reader.readAsDataURL(this.files[0]);
+  }
+})
+
+$("#imageUploadButton").click(() => {
+  var canvas = cropper.getCroppedCanvas();
+
+  if (canvas == null) {
+    alert("Could not upload image. Make sure it is an image file.");
+    return;
+  }
+
+  canvas.toBlob((blob) => {
+    var formData = new FormData();
+    formData.append("croppedImage", blob);
+
+    $.ajax({
+      url: "/api/users/profilePicture",
+      type: "POST",
+      data: formData,
+      processData: false, // Do force JQuery not to convert the formData to a string
+      contentType: false,
+      success: () => {
+        location.reload();
+      }
+    });
+  });
+})
 
 $(document).on("click", ".likeButton", (event) => {
     var button = $(event.target);
