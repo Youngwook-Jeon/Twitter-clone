08a54d557fd3479e0dd2232875f25228d2af1138
Young wook Jeon <ela87@naver.com>
2021-02-02 18:16:27 +0900

    Output the sender name and profile image

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -5,14 +5,21 @@ $(document).ready(() => {
 
     $.get(`/api/chats/${chatId}/messages`, (data) => {
         var messages = [];
+        var lastSenderId = "";
 
-        data.forEach(message => {
-            var html = createMessageHtml(message);
+        data.forEach((message, index) => {
+            var html = createMessageHtml(message, data[index + 1], lastSenderId);
             messages.push(html);
+
+            lastSenderId = message.sender._id;
         });
 
         var messagesHtml = messages.join("");
         addMessagesHtmlToPage(messagesHtml);
+        scrollToBottom(false);
+
+        $(".loadingSpinnerContainer").remove();
+        $(".chatContainer").css("visibility", "visible");
     });
 })
 
@@ -48,8 +55,6 @@ $(".inputTextbox").keydown((event) => {
 
 function addMessagesHtmlToPage(html) {
     $(".chatMessages").append(html);
-
-    // TODO: Scroll to Bottom
 }
 
 function messageSubmitted() {
@@ -78,20 +83,66 @@ function addChatMessageHtml(message) {
         return;
     }
 
-    var messageDiv = createMessageHtml(message);
+    var messageDiv = createMessageHtml(message, null, "");
 
     addMessagesHtmlToPage(messageDiv);
+    scrollToBottom(true);
 }
 
-function createMessageHtml(message) {
+function createMessageHtml(message, nextMessage, lastSenderId) {
+    var sender = message.sender;
+    var senderName = sender.firstName + " " + sender.lastName;
+
+    var currentSenderId = sender._id;
+    var nextSenderId = nextMessage != null ? nextMessage.sender._id : "";
+
+    var isFirst = lastSenderId != currentSenderId;
+    var isLast = nextSenderId != currentSenderId;
+
     var isMine = message.sender._id == userLoggedIn._id;
     var liClassName = isMine ? "mine" : "theirs";
 
+    var nameElement = "";
+
+    if (isFirst) {
+        liClassName += " first";
+
+        if (!isMine) {
+            nameElement = `<span class='senderName'>${senderName}</span>`
+        }
+    }
+
+    var profileImage = "";
+    if (isLast) {
+        liClassName += " last";
+        profileImage = `<img src='${sender.profilePic}' alt='profile image'>`
+    }
+
+    var imageContainer = "";
+    if (!isMine) {
+        imageContainer = `<div class='imageContainer'>
+            ${profileImage}
+        </div>`;
+    }
+
     return `<li class='message ${liClassName}'>
+        ${imageContainer}
         <div class='messageContainer'>
+            ${nameElement}
             <span class='messageBody'>
                 ${message.content}
             </span>
         </div>
     </li>`;
+}
+
+function scrollToBottom(animated) {
+    var container = $(".chatMessages");
+    var scrollHeight = container[0].scrollHeight;
+
+    if (animated) {
+        container.animate({ scrollTop: scrollHeight }, "slow");
+    } else {
+        container.scrollTop(scrollHeight);
+    }
 }
