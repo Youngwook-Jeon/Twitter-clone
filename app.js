0aa0d1fc9bfcd1ee28d2d99bd3946ecf266828fe
Young wook Jeon <ela87@naver.com>
2021-02-03 22:07:52 +0900

    Add Socket Events for Typing on Chat Page

--- app.js
+++ app.js
@@ -63,5 +63,13 @@ app.get("/", middleware.requireLogin, (req, res, next) => {
 });
 
 io.on("connection", (socket) => {
-    console.log("connected to socket io");
+
+    socket.on("setup", userData => {
+        socket.join(userData._id);
+        socket.emit("connected")
+    });
+
+    socket.on("join room", room => socket.join(room));
+    socket.on("typing", room => socket.in(room).emit("typing"));
+    socket.on("stop typing", room => socket.in(room).emit("stop typing"));
 });
